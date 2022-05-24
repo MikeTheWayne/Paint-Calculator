7d3c16be63837a7e0f9dfc1f0c5bc53e1a5130c5
Michael Wayne <mikethewayne@gmail.com>
2022-05-24 22:42:33 +0100

    Add input validation to PaintRequirement
    Input validation has now been implemented in the PaintRequirement class, passing all tests.

--- src/com/michaelwayne/paint/PaintRequirement.java
+++ src/com/michaelwayne/paint/PaintRequirement.java
@@ -13,9 +13,15 @@ public class PaintRequirement {
 	 * @param paintPerSquareMetre The amount of paint required per square metre.
 	 * @param room A populated instance of a room implementation.
 	 */
-	public PaintRequirement(long paintPerSquareMetre, Room room) {
-		// TODO Add input validation
+	public PaintRequirement(long paintPerSquareMetre, Room room) throws IllegalArgumentException {
+		// Validate parameters
+		if(paintPerSquareMetre < 0) {
+			throw new IllegalArgumentException("Paint per square metre should not be negative!");
+		} else if(room == null) {
+			throw new IllegalArgumentException("Room cannot be null!");
+		}
 		
+		// Set values
 		this.paintPerSquareMetre = paintPerSquareMetre;
 		this.room = room;
 	}
