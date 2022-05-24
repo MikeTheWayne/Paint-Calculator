962ca071ab968ab31bfc58ca9ddd59dd89db20b1
Michael Wayne <mikethewayne@gmail.com>
2022-05-24 20:50:26 +0100

    Add tests for Room and RoomFactory
    Added a range of unit tests which the RoomFactory class, and the abstract Room class, must pass when the validation in the Room constructor is implemented.

--- src/com/michaelwayne/tests/RoomFactoryTest.java
+++ src/com/michaelwayne/tests/RoomFactoryTest.java
@@ -1,5 +1,62 @@
 package com.michaelwayne.tests;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.michaelwayne.room.RoomFactory;
+import com.michaelwayne.room.RoomType;
+
 public class RoomFactoryTest {
+	
+	private RoomFactory roomFactory = new RoomFactory();
+
+	@Test
+	public void testRoomFactory_expectedValues_shouldInstantiateCorrectly() {
+		List<Long> dimensions = Arrays.asList(4000L, 5000L, 20L);
+		
+		this.roomFactory.create(RoomType.CUBOID, dimensions);
+	}
+	
+	@Test
+	public void testRoomFactory_noRoomType_shouldInstantiateCorrectly() {
+		List<Long> dimensions = Arrays.asList(4000L, 5000L, 20L);
+		
+		this.roomFactory.create(null, dimensions);
+	}
+	
+	@Test
+	public void testRoomFactory_emptyList_shouldInstantiateCorrectly() {
+		List<Long> dimensions = Arrays.asList();
+		
+		this.roomFactory.create(RoomType.CUBOID, dimensions);
+	}
+	
+	@Test
+	public void testRoomFactory_nullValue_shouldThrow_IllegalArgumentException() {
+		List<Long> dimensions = Arrays.asList(4000L, null, 20L);
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> this.roomFactory.create(RoomType.CUBOID, dimensions));
+	}
+	
+	@Test
+	public void testRoomFactory_nullArray_shouldThrow_IllegalArgumentException() {
+		List<Long> dimensions = null;
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> this.roomFactory.create(RoomType.CUBOID, dimensions));
+	}
+	
+	@Test
+	public void testRoomFactory_negativeArgument_shouldThrow_IllegalArgumentException() {
+		List<Long> dimensions = Arrays.asList(4000L, -5000L, 20L);
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> this.roomFactory.create(RoomType.CUBOID, dimensions));
+	}
 	
 }
