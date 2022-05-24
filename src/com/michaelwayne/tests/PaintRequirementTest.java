ba02cbbb7d22ed6e8d0c228438e32075dca126e2
Michael Wayne <mikethewayne@gmail.com>
2022-05-24 22:38:09 +0100

    Add PaintRequirement input validation tests
    Tests have been added for the PaintRequirement class, in order to ensure that the input validation which will be implemented there is implemented correctly.

---
+++ src/com/michaelwayne/tests/PaintRequirementTest.java
@@ -0,0 +1,40 @@
+package com.michaelwayne.tests;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.michaelwayne.paint.PaintRequirement;
+import com.michaelwayne.room.Room;
+import com.michaelwayne.room.RoomFactory;
+import com.michaelwayne.room.RoomType;
+
+public class PaintRequirementTest {
+	
+	private RoomFactory roomFactory = new RoomFactory();
+	
+	private List<Long> dimensions = Arrays.asList(4000L, 5000L, 20L);
+	
+	private Room room = roomFactory.create(RoomType.CUBOID, dimensions);
+	
+	@Test
+	public void testPaintRequirement_expectedValues_shouldInstantiateCorrectly() {
+		new PaintRequirement(5L, room);
+	}
+	
+	@Test
+	public void testPaintRequirement_negativePaintPerSquareMetre_shouldThrow_IllegalArgumentException() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new PaintRequirement(-5L, room));
+	}
+	
+	@Test
+	public void testPaintRequirement_nullRoom_shouldThrow_IllegalArgumentException() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new PaintRequirement(5L, null));
+	}
+	
+}
